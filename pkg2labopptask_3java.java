2bfa6dbf8d06e6b7f01b64395e5ec0714316a390
sabeer116 <sabeerahmad143@gmail.com>
2025-02-18 21:22:14 +0500

    Add files via upload
    2.	Write a program to print a 5x5 pattern of stars (*) using nested for loops.

---
+++ pkg2labopptask_3java.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Dell
+ */
+public class pkg2labopptask_3java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int size = 5; 
+
+        
+        for (int i = 0; i < size; i++) {
+            
+            for (int j = 0; j < size; j++) {
+                System.out.print("* "); 
+            }
+            System.out.println(); 
+        }
+    }
+}
+    
+    
+
