1c1112bdf421e97b2f6f2836ed9ee085d6165a41
sabeer116 <sabeerahmad143@gmail.com>
2025-02-18 21:24:35 +0500

    Add files via upload
    4.	Write a program to perform arithmetic operations (addition, subtraction, multiplication, division

---
+++ pkg2labopptask_6java.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Dell
+ */
+public class pkg2labopptask_6java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x, y, sum, sub, mult, div;
+        x=8;
+        y=4;
+        sum=x+y;
+        sub=x-y;
+        mult=x*y;
+        div=x/y;
+        System.out.println("addition:" +sum);
+        System.out.println("subtraction:" +sub);
+        System.out.println("multiplication:" +mult);
+        System.out.println("division:" +div);
+      
+    }
+    
+}
