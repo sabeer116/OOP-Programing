f8e8f27daa6b466e210ac8c7023f36d983244180
sabeer116 <sabeerahmad143@gmail.com>
2025-02-18 21:25:35 +0500

    Add files via upload
    5.	Write a program to find the sum of all even numbers between 1 and 100 using a for loop and an if statement.

---
+++ pkg2labopptask_5java.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Dell
+ */
+public class pkg2labopptask_5java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int sum = 0; 
+
+    
+        for (int i = 1; i <= 100; i++) {
+            
+            if (i % 2 == 0) {
+                sum += i; 
+            }
+        }
+
+         
+        System.out.println("Sum of all even numbers between 1 and 100: " + sum);
+    }
+}
+
+    
+    
+
