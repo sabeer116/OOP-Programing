3517cc59dd42921f6856f7cf7dcaa884c44d8932
sabeer116 <sabeerahmad143@gmail.com>
2025-03-05 22:07:18 +0500

    Add files via upload

---
+++ Labopptask_5java2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labopptask_5java.pkg2;
+
+/**
+ *
+ * @author Dell
+ */
+public class Labopptask_5java2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+           double x, y, sum, sub, mult, div;
+        x=8;
+        y=4;
+        sum=x+y;
+        sub=x-y;
+        mult=x*y;
+        div=x/y;
+        System.out.println("addition:" +sum);
+        System.out.println("subtraction:" +sub);
+        System.out.println("multiplication:" +mult);
+        System.out.println("division:" +div);
+    }
+    
+}
